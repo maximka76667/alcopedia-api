9692004fed31685c756655a58d458dcc1bb92bb3
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-09-11 00:41:45 +0600

    cors fix

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
 const { LOGIN, PASSWORD } = process.env;
 
+const ALLOWED_CORS = [
+  'localhost:3000',
+  'http://localhost:3000',
+  'localhost:3001',
+  'http://localhost:3001',
+  'localhost:5000',
+  'http://localhost:5000',
+  'https://maximka76667.github.io',
+  'maximka76667.github.io',
+];
+const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS';
 const PORT = 3001;
 const DB_URL = `mongodb+srv://${LOGIN}:${PASSWORD}@cluster0.h9ihfyc.mongodb.net/?retryWrites=true&w=majority`
 
-module.exports = { DB_URL, PORT };
+module.exports = { ALLOWED_CORS, DEFAULT_ALLOWED_METHODS, DB_URL, PORT };
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const express = require('express')
 const app = express() // notice that the app instance is called `app`, this is very important.
 
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-const { DB_URL, PORT } = require("./config");
+const { ALLOWED_CORS, DEFAULT_ALLOWED_METHODS, DB_URL, PORT } = require("./config");
 
 const connectDB = async () => {
   await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -12,6 +13,21 @@ const connectDB = async () => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (ALLOWED_CORS.includes(origin)) return callback(null, true);
+      return callback(new Error('Ошибка CORS'), true);
+    },
+    methods: DEFAULT_ALLOWED_METHODS,
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  }),
+);
 
 app.use("/drink", require('./routes/index'));
 

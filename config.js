ed39333a2a6cf801b52fef7fe6e69288a988d90f
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-09-18 11:41:09 +0600

    delete extra ips

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
 const { LOGIN, PASSWORD } = process.env;
 
 const ALLOWED_CORS = [
-  'localhost:3000',
   'http://localhost:3000',
-  'localhost:3001',
   'http://localhost:3001',
-  'localhost:5000',
   'http://localhost:5000',
   'https://maximka76667.github.io',
-  'maximka76667.github.io',
   'http://192.168.1.46:3000'
 ];
 const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS';

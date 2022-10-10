e16594237e6242ec794917658997855478757508
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-10-10 18:43:47 +0200

    update allowed urls

--- config/cors.js
+++ config/cors.js
@@ -1,9 +1,8 @@
 const ALLOWED_CORS = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'http://localhost:5000',
   'https://maximka76667.github.io',
-  'http://192.168.1.46:3000',
+  'http://192.168.1.89:3000',
 ];
 const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS';
 

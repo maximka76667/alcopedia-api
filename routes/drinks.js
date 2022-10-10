54a180a8e03a3e1e716b14990776794a3c8a8e10
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-10-10 18:57:56 +0200

    fix routes

--- routes/drinks.js
+++ routes/drinks.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 
-const { getDrinks, addDrink, deleteDrink } = require('../controllers/drinks');
+const { addDrink, deleteDrink } = require('../controllers/drinks');
 
-router.get('/', getDrinks);
 router.post('/', addDrink);
 router.delete('/', deleteDrink);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
+const { getDrinks } = require('../controllers/drinks');
 const { login } = require('../controllers/users');
 
 router.post('/login', login);
+router.get('/drink', getDrinks);
 router.use(require('../middlewares/auth'));
 router.use('/', require('./users'));
 router.use('/drink', require('./drinks'));
